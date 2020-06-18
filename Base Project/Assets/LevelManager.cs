8b49b2cbb1880623ee7c27a52f06d2a74b3cefac
lyqht <estee.tsw@gmail.com>
2020-06-18 12:08:40 +0800

    added cinematomachine package for camera to follow player. added levelmanager and object pooler

---
+++ Base Project/Assets/LevelManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    private int currentLevel;
+    public int numLevels;
+    public static LevelManager Instance;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentLevel = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GoToNextLevel()
+    {
+        currentLevel += 1;
+    }
+}
---
+++ Base Project/Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class ObjectPooler : MonoBehaviour
+{   
+    [System.Serializable]
+    public class Pool
+    {
+        public string tag;
+        public GameObject prefab;
+        public int size;
+    }
+
+    public List<Pool> pools;
+    public Dictionary<string, Queue<GameObject>> poolDictionary;
+    public static ObjectPooler Instance = null;
+
+    public int numObstaclesSpawned;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        
+    }
+
+    void Start()
+    {
+        poolDictionary = new Dictionary<string, Queue<GameObject>>();
+        foreach (Pool pool in pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+            for (int i = 0; i < pool.size; i++)
+            {
+                GameObject obj = Instantiate(pool.prefab);
+                obj.SetActive(false);
+                objectPool.Enqueue(obj);
+            }
+
+            poolDictionary.Add(pool.tag, objectPool);
+        }
+    }
+
+    public GameObject SpawnFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        numObstaclesSpawned += 1;
+        Debug.Log("Number obstacles spawned: " + numObstaclesSpawned);
+
+        if (poolDictionary.ContainsKey(tag))
+        {
+            GameObject objectToSpawn = poolDictionary[tag].Dequeue();
+            objectToSpawn.SetActive(true);
+            objectToSpawn.transform.position = position;
+            objectToSpawn.transform.rotation = rotation;
+            poolDictionary[tag].Enqueue(objectToSpawn);
+            return objectToSpawn;
+        }
+        else
+        {
+            Debug.LogWarning("Pool with tag" + tag + " does not exist");
+            return null;
+        }
+    }
+}
