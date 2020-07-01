014354fcdc07c44cf6c20b262d7831dbdc495876
Dominic Cordeiro <dominiccordeiro@gmail.com>
2020-07-01 22:17:08 +0800

    added basic enemy class and placeholder animations

---
+++ Base Project/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+
+public class BasicEnemy : Enemy
+{
+    // Start is called before the first frame update
+    public Transform[] moveSpots;
+    private int randomSpot;
+    private float waitTime;
+    public float startWaitTime;
+    private Animator animator;
+    SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        animator = GetComponent<Animator>();
+        randomSpot = Random.Range(0, moveSpots.Length);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // move towards x position only, ignoring y positions
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(moveSpots[randomSpot].position.x, transform.position.y), moveSpeed * Time.deltaTime);
+
+
+        // check if it has reached the spot (with some error margin), if yes, then move to another random location
+        if (Vector2.Distance(transform.position, new Vector2(moveSpots[randomSpot].position.x, transform.position.y)) < 0.02f)
+        {
+            // wait for awhile
+            if (waitTime <= 0)
+            {
+                // do something
+                randomSpot = Random.Range(0, moveSpots.Length); // move to a new random location
+                waitTime = startWaitTime; // reset timer 
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+
+        // animation logic, directional checks implemented in base class
+        if (IsLeft(transform.position.x, moveSpots[randomSpot].position.x))
+        {
+            animator.Play("ice_zombie_run"); // go right
+            spriteRenderer.flipX = false;
+        }
+
+        else if (IsRight(transform.position.x, moveSpots[randomSpot].position.x))
+        {
+            animator.Play("ice_zombie_run"); // go left
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            animator.Play("ice_zombie_idle");
+        }
+    }
+}
---
+++ Base Project/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public bool isHit;
+    public float moveSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool IsLeft(float CurrentPoint, float TargetPoint)
+    {
+        if (CurrentPoint < TargetPoint)
+        {
+            return true;
+        }
+        return false;
+    }
+    public bool IsRight(float CurrentPoint, float TargetPoint)
+    {
+        if (CurrentPoint > TargetPoint)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    // can add IsAbove or IsBelow
+}
