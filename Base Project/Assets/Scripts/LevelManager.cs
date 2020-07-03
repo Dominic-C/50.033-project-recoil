9ff1a9cb4c93f89334abdc044a432786ff090169
lyqht <estee.tsw@gmail.com>
2020-07-03 17:38:35 +0800

    tutorial stage 3 map done

--- Base Project/Assets/Scripts/LevelManager.cs
+++ Base Project/Assets/Scripts/LevelManager.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour
 {
     public static LevelManager Instance;
     private int currentLevel;
-    public int numLevels;
-
-    private float maxDistance;
-    private float currentDistance; // affected by how much player moves from starting point of the level.
-    public Slider progressSlider; // UI to show how much the player has progressed in the level.
-    public Image progressFill;
-    public Gradient progressColorGradient;
+    public Vector3 playerSpawnPosition;
 
     void Awake()
     {
@@ -31,11 +26,12 @@ public class LevelManager : MonoBehaviour
         currentDistance = 0;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Progress UI related functions
+    private float maxDistance;
+    private float currentDistance; // affected by how much player moves from starting point of the level.
+    public Slider progressSlider; // UI to show how much the player has progressed in the level.
+    public Image progressFill;
+    public Gradient progressColorGradient;
 
     public void SetLevelProgress (float distance)
     {
