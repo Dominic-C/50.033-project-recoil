421506d0b91cbade58b79301264e1f11c206b072
Dominic Cordeiro <dominiccordeiro@gmail.com>
2020-07-03 22:18:18 +0800

    add basic enemy agro with line of sight

--- Base Project/Assets/Scripts/Enemy/BasicEnemy.cs
+++ Base Project/Assets/Scripts/Enemy/BasicEnemy.cs
@@ -7,58 +7,95 @@ public class BasicEnemy : Enemy
 {
     // Start is called before the first frame update
     public Transform[] moveSpots;
+    public float startWaitTime;
+    SpriteRenderer spriteRenderer;
+    
+    private Rigidbody2D rb2d;
     private int randomSpot;
     private float waitTime;
-    public float startWaitTime;
     private Animator animator;
-    SpriteRenderer spriteRenderer;
-
+    
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
         waitTime = startWaitTime;
         animator = GetComponent<Animator>();
         randomSpot = Random.Range(0, moveSpots.Length);
         spriteRenderer = GetComponent<SpriteRenderer>();
+        isFacingLeft = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        // move towards x position only, ignoring y positions
-        transform.position = Vector2.MoveTowards(transform.position, new Vector2(moveSpots[randomSpot].position.x, transform.position.y), moveSpeed * Time.deltaTime);
-
-
-        // check if it has reached the spot (with some error margin), if yes, then move to another random location
-        if (Vector2.Distance(transform.position, new Vector2(moveSpots[randomSpot].position.x, transform.position.y)) < 0.02f)
+        if (canSeePlayer(rangeOfSight))
         {
-            // wait for awhile
-            if (waitTime <= 0)
-            {
-                // do something
-                randomSpot = Random.Range(0, moveSpots.Length); // move to a new random location
-                waitTime = startWaitTime; // reset timer 
-            }
-            else
-            {
-                waitTime -= Time.deltaTime;
-            }
-        }
-
-        // animation logic, directional checks implemented in base class
-        if (IsLeft(transform.position.x, moveSpots[randomSpot].position.x))
-        {
-            animator.Play("ice_zombie_run"); // go right
-            spriteRenderer.flipX = false;
-        }
-
-        else if (IsRight(transform.position.x, moveSpots[randomSpot].position.x))
-        {
-            animator.Play("ice_zombie_run"); // go left
-            spriteRenderer.flipX = true;
+            spriteRenderer.color = Color.red;
+            chasePlayer();
         }
         else
         {
-            animator.Play("ice_zombie_idle");
+            spriteRenderer.color = Color.white;
+            
+            // =============================== Patrol logic ======================================
+            // move towards x position only, ignoring y positions
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(moveSpots[randomSpot].position.x, transform.position.y), moveSpeed * Time.deltaTime);
+
+        
+            // check if it has reached the spot (with some error margin), if yes, then move to another random location
+            if (Vector2.Distance(transform.position, new Vector2(moveSpots[randomSpot].position.x, transform.position.y)) < 0.02f)
+            {
+                // wait for awhile
+                if (waitTime <= 0)
+                {
+                    // do something
+                    randomSpot = Random.Range(0, moveSpots.Length); // move to a new random location
+                    waitTime = startWaitTime; // reset timer 
+                }
+                else
+                {
+                    waitTime -= Time.deltaTime;
+                }
+            }
+
+            // ================================ Line of sight and animation ==================================
+            if (isLeft(transform.position.x, moveSpots[randomSpot].position.x))
+            {
+                animator.Play(runAnimationClip.name);
+                spriteRenderer.flipX = false;
+                isFacingLeft = false;
+            }
+
+            else if (isRight(transform.position.x, moveSpots[randomSpot].position.x))
+            {
+                animator.Play(runAnimationClip.name);
+                spriteRenderer.flipX = true;
+                isFacingLeft = true;
+            }
+            else
+            {
+                animator.Play(idleAnimationClip.name);
+            }
+        
         }
+    }
+
+    void chasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            animator.Play(runAnimationClip.name);
+            spriteRenderer.flipX = false;
+            isFacingLeft = false;
+            rb2d.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            animator.Play(runAnimationClip.name);
+            spriteRenderer.flipX = true;
+            isFacingLeft = true;
+            rb2d.velocity = new Vector2(-moveSpeed, 0);
+        }
+        
     }
 }
--- Base Project/Assets/Scripts/Enemy/Enemy.cs
+++ Base Project/Assets/Scripts/Enemy/Enemy.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public bool isHit;
+    public Transform player;
+    public Transform eyes;
     public float moveSpeed;
+    public float rangeOfSight;
+    public AnimationClip idleAnimationClip;
+    public AnimationClip runAnimationClip;
+    protected bool isFacingLeft;
+    public bool isHit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +26,7 @@ public class Enemy : MonoBehaviour
         
     }
 
-    public bool IsLeft(float CurrentPoint, float TargetPoint)
+    public bool isLeft(float CurrentPoint, float TargetPoint)
     {
         if (CurrentPoint < TargetPoint)
         {
@@ -26,7 +34,7 @@ public class Enemy : MonoBehaviour
         }
         return false;
     }
-    public bool IsRight(float CurrentPoint, float TargetPoint)
+    public bool isRight(float CurrentPoint, float TargetPoint)
     {
         if (CurrentPoint > TargetPoint)
         {
@@ -35,5 +43,49 @@ public class Enemy : MonoBehaviour
         return false;
     }
 
-    // can add IsAbove or IsBelow
+    // can also add IsAbove or IsBelow if required, but the idea is the same
+
+    public bool canSeePlayer(float distance)
+    {
+        bool val = false;
+        float castDist = distance;
+
+        if (isFacingLeft)
+        {
+            castDist = -distance;
+        }
+        
+        Vector2 endPos = eyes.position + Vector3.right * castDist;
+
+        // ray intercepts with things with the layer tag "Action" only. goes through all other objects.
+        RaycastHit2D hit = Physics2D.Linecast(eyes.position, endPos, 1 << LayerMask.NameToLayer("Action"));
+
+        if (hit.collider != null)
+        {
+            // check if the ray hits the player
+            if (hit.collider.gameObject.CompareTag("Player"))
+            {
+                val = true;
+            }
+            else
+            {
+                val = false;
+            }
+        }
+
+        if (!val)
+        {
+            // cannot see player
+            Debug.DrawLine(eyes.position, eyes.position + Vector3.right * castDist, Color.blue);
+        }
+        else
+        {
+            // can see player
+            Debug.DrawLine(eyes.position, eyes.position + Vector3.right * castDist, Color.yellow);
+        }
+        return val;
+
+    }
+
+
 }
