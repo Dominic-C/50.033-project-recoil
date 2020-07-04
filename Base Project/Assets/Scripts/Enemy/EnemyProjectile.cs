da90928771b1a96e6a6c4ae6bce43b32042a7732
Dominic Cordeiro <dominiccordeiro@gmail.com>
2020-07-04 15:07:35 +0800

    added basic shooting enemy

---
+++ Base Project/Assets/Scripts/Enemy/EnemyProjectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectile : MonoBehaviour
+{
+    public bool isFacingLeft;
+    public float projectileSpeed;
+    // Start is called before the first frame update
+    private Rigidbody2D rb2d;
+    private Transform player;
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (isFacingLeft)
+        {
+            projectileSpeed = -projectileSpeed;
+        }
+        rb2d.velocity = transform.TransformDirection(new Vector2(projectileSpeed, 0));
+    }
+}
---
+++ Base Project/Assets/Scripts/Enemy/EnemyShooter.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShooter : Enemy
+{
+    SpriteRenderer spriteRenderer;
+
+    private Rigidbody2D rb2d;
+    private int randomSpot;
+    private float waitTime;
+    private Animator animator;
+
+    // variables for shooting
+    private float timeBetweenShots;
+    public float startTimeBetweenShots; // seconds
+    public GameObject projectile;
+    private bool playerInSight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        isFacingLeft = false;
+        timeBetweenShots = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // player in range
+        playerInSight = canSeePlayer(rangeOfSight);
+        if (playerInSight)
+        {
+            spriteRenderer.color = Color.red;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+        // timed shots
+        if (timeBetweenShots <=0)
+        {
+            if (playerInSight)
+            {
+                // shoot projectile
+                GameObject projectileObject = Instantiate(projectile, eyes.position, Quaternion.identity); // instantiate with initial position and rotation
+                projectileObject.GetComponent<EnemyProjectile>().isFacingLeft = isFacingLeft;
+                timeBetweenShots = startTimeBetweenShots;
+            }
+        }
+        else
+        {
+            // "recharge" even if player not in range
+            timeBetweenShots -= Time.deltaTime;
+        }
+     }
+}
+
