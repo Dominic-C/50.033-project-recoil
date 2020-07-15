ee062cd074f351738b7a2ee33b4592f640f1284f
lyqht <siewwen_tey@mymail.sutd.edu.sg>
2020-07-15 22:29:10 +0800

    Energy Core Pickup Prefab, and making of other pickups as Prefabs

--- Base Project/Assets/Scripts/Pickup.cs
+++ Base Project/Assets/Scripts/Pickup.cs
@@ -3,32 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Build;
 using UnityEngine;
+using UnityEngine.Events;
 
 public enum PickupType
 {
     Weapon, 
     Hint,
-    EasterEgg
+    EasterEgg,
+    AmmoRefill
 }
 
 public class Pickup : MonoBehaviour
 {
     public PickupType type;
     public GameObject linkedObject;
+    public UnityEvent onEnter;
 
     void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            linkedObject.SetActive(true);
+            if (linkedObject) linkedObject.SetActive(true);
             gameObject.SetActive(false); // to be replaced with some animation of disappearing
-
+            if (onEnter != null) onEnter.Invoke();
         }
     }
 
     void Awake()
     {
-        linkedObject.SetActive(false);
+        if (linkedObject != null) linkedObject.SetActive(false);
     }
 
 }
