2b7e38fa27a71f4805aa9b3f6f110d8e9a05da4f
Gui An Lee <piroton@users.noreply.github.com>
2020-07-16 13:36:56 +0800

    Add Stage 1-1 and Stage 1-2 to game; add camera follower

---
+++ Base Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    private Vector3 playerPos;
+    private Vector3 offsetPos = new Vector3(0, 0, -10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerPos = Player.GetComponent<Transform>().position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = Player.GetComponent<Transform>().position;
+        transform.position = playerPos + offsetPos;
+    }
+}
