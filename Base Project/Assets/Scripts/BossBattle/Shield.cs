4d16a03ce5cdb0e18176307ecc64321f8b7cfd06
Dominic Cordeiro <dominiccordeiro@gmail.com>
2020-07-28 15:58:38 +0800

    add battery and shield scripts

---
+++ Base Project/Assets/Scripts/BossBattle/Shield.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject[] batteries;
+    private int energyLevel;
+
+    void Start()
+    {
+        energyLevel = batteries.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        energyLevel = 0;
+        for (int i =0; i < batteries.Length; i++)
+        {
+            if (!batteries[i].GetComponent<BatteryStatus>().isHit)
+            {
+                energyLevel += 1;
+            }
+        }
+
+        if (energyLevel <= 0)
+        {
+            KillSelf();
+        }
+    }
+
+    private void KillSelf()
+    {
+        Destroy(gameObject);
+    }
+}
