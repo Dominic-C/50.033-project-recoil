a96bb78cd1723b624cb0884de0b9f1e6322f69d5
lyqht <estee.tsw@gmail.com>
2020-08-01 21:09:15 +0800

    added GameResults Scene
    - to test this, you can try adding the TestEgg scene right before it and play through that scene.
    - otherwise you can play through entire game, should work the same.

---
+++ Base Project/Assets/GameResults.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameResults : MonoBehaviour
+{
+    public GameObject EggHorizontalLayout;
+    public TextMeshProUGUI TimeText;
+    public Sprite RedEgg;
+    public Sprite BlueEgg;
+    public Sprite GreenEgg;
+    public Image image;
+
+    void Start()
+    {
+        foreach (int egg in LevelManager.EggsCollected)
+        {
+            Image img = Instantiate(image, EggHorizontalLayout.transform);
+            switch (egg)
+            {
+                case (int) EggType.RED:
+                    img.sprite = RedEgg;
+                    break;
+                case (int) EggType.BLUE:
+                    img.sprite = BlueEgg;
+                    break;
+                case (int) EggType.GREEN:
+                    img.sprite = GreenEgg;
+                    break;
+            }
+        }
+
+        float totalTimeTaken = LevelManager.timeTakenPerStage.Values.Sum();
+        TimeText.text = totalTimeTaken.ToString("n2") + " s";
+    }
+}
