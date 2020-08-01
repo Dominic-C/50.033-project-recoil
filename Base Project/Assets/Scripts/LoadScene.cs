033684d059a314f94c9a0b793912d6005d1e8baf
Maruth Nath Chaowanasatian <mcnath45@gmail.com>
2020-08-01 17:30:45 +0800

    Fix issues with cutscene and add it to build

---
+++ Base Project/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public Button skipButton;
+    
+    void Start(){
+        OnEnable();
+    }
+    public static void OnEnable()
+    {
+        Debug.Log("Going to next scene");
+    
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
