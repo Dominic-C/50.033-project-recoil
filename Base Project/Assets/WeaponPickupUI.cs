19465ccfb99135189aa7bf17758cc4552681a442
lyqht <estee.tsw@gmail.com>
2020-08-03 17:07:26 +0800

    added weaponPickup Canvas  and hint note for rocket pickup
    changes are made to tutorialstage 2 and stage 2-3

---
+++ Base Project/Assets/WeaponPickupUI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponPickupUI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public WeaponData weaponData;
+    public Image weaponImage;
+    public TextMeshProUGUI weaponNameText;
+
+
+    void Start()
+    {
+        weaponImage.sprite = weaponData.weaponImage;
+        weaponNameText.text = weaponData.weaponName;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
