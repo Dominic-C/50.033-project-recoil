f21afdf4c69b8611b6c781f2b4b607f3519a80ed
lyqht <estee.tsw@gmail.com>
2020-08-09 22:58:07 +0800

    add penguin to mainmenu

---
+++ Base Project/Assets/MainMenuPenguin.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuPenguin : MonoBehaviour
+{
+    public Sprite Reaction;
+    private Sprite originalExpression;
+    private Image renderedImage;
+
+    void Start()
+    {
+        Debug.Log("MainMenu Penguin!");
+        renderedImage = GetComponent<Image>();
+        originalExpression = renderedImage.sprite;
+    }
+
+    public void changeExpression()
+    {
+        Debug.Log("Changing Expression");
+        StartCoroutine(ChangeExpression());
+
+    }
+
+    IEnumerator ChangeExpression()
+    {
+        renderedImage.sprite = Reaction;
+        yield return new WaitForSeconds(1f);
+        renderedImage.sprite = originalExpression;
+    }
+}
