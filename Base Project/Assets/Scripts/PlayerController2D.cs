4da0f5564aa5e72a658d2c2f6d0d6540e301dcaa
Gui An Lee <piroton@users.noreply.github.com>
2020-08-10 18:20:48 +0800

    hopefully fix the UI pause
    playercontroller a shit

--- Base Project/Assets/Scripts/PlayerController2D.cs
+++ Base Project/Assets/Scripts/PlayerController2D.cs
@@ -68,7 +68,7 @@ public class PlayerController2D : MonoBehaviour
     {
         // Controller Mode
         // Check For Pause State
-        if (!LevelManager.GameIsPaused && isAlive)
+        if (!LevelManager.GameIsPaused && isAlive && LevelManager.InputOn)
         {
             // Get Inputs;
             rightPressed = Input.GetKey("d");
