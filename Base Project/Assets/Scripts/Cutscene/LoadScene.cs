0cfff50df8ed4d840c0a8d3a8a88cc257ff2549a
lyqht <estee.tsw@gmail.com>
2020-08-14 22:38:30 +0800

    fix extraBenedict at GameResults bug

--- Base Project/Assets/Scripts/Cutscene/LoadScene.cs
+++ Base Project/Assets/Scripts/Cutscene/LoadScene.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -10,11 +8,11 @@ public class LoadScene : MonoBehaviour
     public bool addBenedict = false;
     
     void Start(){
-        OnEnable();
-    }
-    public static void OnEnable()
-    {
-        LevelManager.EggsCollected.Add(4);
+        if (addBenedict)
+        {
+            LevelManager.EggsCollected.Add(4);
+
+        }
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
